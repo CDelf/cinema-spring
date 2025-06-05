43f4248ea06b64cbc25915f63fc88030b0b5e0e3
Carole <c.delfino@live.fr>
2025-06-05 14:46:50 +0200

    Commit initial

---
+++ src/test/java/fr/diginamic/cinemaspring/CinemaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.cinemaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

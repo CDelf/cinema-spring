b69954784503282d4df56b39ab7fe9d3dc3b823d
Carole <c.delfino@live.fr>
2025-06-05 16:58:43 +0200

    ExceptionHandler

---
+++ src/main/java/fr/diginamic/cinemaspring/exceptions/FunctionalException.java
@@ -0,0 +1,8 @@
+package fr.diginamic.cinemaspring.exceptions;
+
+public class FunctionalException extends Exception {
+
+    public FunctionalException(String message) {
+        super(message);
+    }
+}
